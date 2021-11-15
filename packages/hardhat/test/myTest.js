b26583cf404b8ed205b30cedcae03296d4c30f0c
Daven <daps2182gmail.com>
2021-11-15 18:41:58 +0300

    added 5 working unit tests

--- packages/hardhat/test/myTest.js
+++ packages/hardhat/test/myTest.js
@@ -1,26 +1,65 @@
 const { ethers } = require("hardhat");
-const { use, expect } = require("chai");
+const { use, expect, assert } = require("chai");
 const { solidity } = require("ethereum-waffle");
+const { not } = require("ramda");
+
 
 use(solidity);
 
 describe("My Dapp", function () {
   let myContract;
 
-  describe("YourContract", function () {
-    it("Should deploy YourContract", async function () {
-      const YourContract = await ethers.getContractFactory("YourContract");
+  before(async () => {
+    contract =  await ethers.getContractFactory("YourCollectible");
+  })
 
-      myContract = await YourContract.deploy();
-    });
+  describe('deployment', async () => {
+    it('deploys successfully', async () => {
+      const address = contract.address
+      assert.notEqual(address, 0x0)
+    })
+  })
 
-    describe("setPurpose()", function () {
-      it("Should be able to set a new purpose", async function () {
-        const newPurpose = "Test Purpose";
+  before(async () => {
+    contract =  await ethers.getContractFactory("YourCollectible");
+  })
 
-        await myContract.setPurpose(newPurpose);
-        expect(await myContract.purpose()).to.equal(newPurpose);
-      });
-    });
-  });
-});
+  describe('deployment', async () => {
+    it('deploys successfully', async () => {
+      const address = contract.address
+      assert.equal(address, undefined)
+    })
+  })
+
+  describe('deployment', async () => {
+    it('deploys successfully', async () => {
+      const address = contract.address
+      assert.equal(address, null)
+    })
+})
+
+describe('deployment', async () => {
+  it('deploys successfully', async () => {
+    const tokenURI = contract.tokenURI
+    assert.equal(tokenURI, null)
+  })
+})
+
+describe('deployment', async () => {
+  it('deploys successfully', async () => {
+    const tokenURI = contract.tokenURI
+    assert.notEqual(tokenURI > 0)
+  })
+
+it('should be able to update the uri', async function() {
+  const tokenURI = contract.setTokenURI
+  await contract.setTokenURI('foo')
+  assert.equal(await contract.baseTokenURI(), 'foo')
+})
+})
+})
+
+
+      // assert.notEqual(address, '')
+      // assert.notEqual(address, null)
+      // assert.notEqual(address, undefined)
